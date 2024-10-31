22065392ab64c9dc5953af501abaacff9cf43420
Victor Schweitz <victorschweitz@hotmail.com>
2024-10-31 20:03:52 +0100

    Icons added

--- src/app/components/CookiePopupContainer.jsx
+++ src/app/components/CookiePopupContainer.jsx
@@ -1,8 +1,12 @@
+import { MdOutlineCookie } from "react-icons/md"
+import { IoMdClose } from "react-icons/io"
+
 const CookiePopupContainer = () =>
 {
     return (
-        <article className="h-[300] w-[250] bg-black rounded-2xl absolute bottom-10 right-10">
-          
+        <article className="h-[300] w-[250] bg-black rounded-2xl shadow-xl relative">
+          <MdOutlineCookie size={50} className="absolute top-5 left-5" />
+          <IoMdClose size={30} className="absolute right-3 top-4 cursor-pointer" />
         </article>        
     )
 }
